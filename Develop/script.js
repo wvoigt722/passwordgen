b76c753ef967d8a230b8cd850e6a54da3f7d8ffc
unknown <wvoigt722@gmail.com>
2022-11-30 15:13:35 -0800

    started working on js code

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,23 @@
 // create variables to hold the character options for special, lowercase, uppercase, and numeric characters.
 
+var generateBtn = document.querySelector("#generate");
+var lowerArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var upperArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var symbolsArray = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
 // create an empty array that will hold the users selected characters.
 
-// var possibleChars = []
-// var passwordArray = []
+var possibleChars = []
+var passwordArray = []
 
 // on game start prompt for users number of characters and hold in variable.
-// var numChars = prompt("How many characters would you like in your password?")
+
+var numChars = prompt("How many characters would you like in your password?")
+
+
+
 
 // check the users input and confirm they put in a number between 8 - 128
 // write function that validates user input and that is it numeric
@@ -52,13 +63,10 @@
 
 // push the randomly generated character to an empty array called passwordArray
 
+//return passwordArray.join()
+
 
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var lowerArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var upperArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var symbolsArray = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 
 
